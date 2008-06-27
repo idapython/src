a8f66766232357d7cd8637c9cfb442c33df4b763
gergely.erdelyi <gergely.erdelyi@fccdda4b-c33c-0410-84de-61e1e3e5f415>
2008-06-27 21:21:38 +0000

    Added small test scripts for colours.

---
+++ examples/colours.py
@@ -0,0 +1,19 @@
+#---------------------------------------------------------------------
+# Colour test
+#
+# This script demonstrates the usage of background colours.
+#
+# Author: Gergely Erdelyi <dyce@d-dome.net>
+#---------------------------------------------------------------------
+
+# Set the colour of the current segment to BLUE
+SetColor(here(), CIC_SEGM, 0xc02020)
+# Set the colour of the current function to GREEN
+SetColor(here(), CIC_FUNC, 0x208020)
+# Set the colour of the current item to RED
+SetColor(here(), CIC_ITEM, 0x2020c0)
+
+# Print the colours just set
+print "%x" % GetColor(here(), CIC_SEGM)
+print "%x" % GetColor(here(), CIC_FUNC)
+print "%x" % GetColor(here(), CIC_ITEM)
