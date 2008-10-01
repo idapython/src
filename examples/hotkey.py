b4552cb6768b693ed361e70830f193fa90132b9d
gergely.erdelyi <gergely.erdelyi@fccdda4b-c33c-0410-84de-61e1e3e5f415>
2008-10-01 15:42:09 +0000

    Added hotkey example script

---
+++ examples/hotkey.py
@@ -0,0 +1,24 @@
+#---------------------------------------------------------------------
+# This script demonstrates the usage of hotkeys.
+#
+# Note: Hotkeys only work with the GUI version of IDA and not in
+#       text mode.
+#
+# Author: Gergely Erdelyi <dyce@d-dome.net>
+#---------------------------------------------------------------------
+import idaapi
+
+def foo():
+ print "Hotkey activated!"
+
+# IDA binds hotkeys to IDC functions so a trampoline IDC function
+# must be created
+idaapi.CompileLine('static key_2() { RunPythonStatement("foo()"); }')
+# Add the hotkey
+AddHotkey("2", 'key_2')
+
+# Press 2 to activate foo()
+
+# The hotkey can be removed with
+# DelHotkey('2')
+
