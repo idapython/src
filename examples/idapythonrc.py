419a08135a3d66e9a12477c8c2d9f6076cbaa01d
gergely.erdelyi <gergely.erdelyi@fccdda4b-c33c-0410-84de-61e1e3e5f415>
2009-01-27 18:09:23 +0000

    Added example user init script

---
+++ examples/idapythonrc.py
@@ -0,0 +1,15 @@
+#---------------------------------------------------------------------
+# Example user initialisation script: idapythonrc.py
+#
+# Place this script to ~/.idapro/ or to 
+# C:\Documents and Settings\%USER%\Application Data\Hex-Rays\IDA Pro
+#---------------------------------------------------------------------
+
+# Add your favourite script to ScriptBox for easy access
+# scriptbox.addscript("/here/is/my/favourite/script.py")
+
+# Uncomment if you want to set Python as default interpreter in IDA
+# idaapi.enable_extlang_python(True)
+
+# Disable the Python from interactive command-line
+# idaapi.enable_python_cli(False)
