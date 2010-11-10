197a94f5d723ad5ebf9a58ced7edb53d81db3105
elias.bachaalany <elias.bachaalany@fccdda4b-c33c-0410-84de-61e1e3e5f415>
2010-11-10 14:05:29 +0000

    Added PySide and PyQt examples

---
+++ examples/ex_pyside.py
@@ -0,0 +1,34 @@
+from idaapi import PluginForm
+from PySide import QtGui, QtCore
+
+class MyPluginFormClass(PluginForm):
+    def OnCreate(self, form):
+        """
+        Called when the plugin form is created
+        """
+
+        # Get parent widget
+        self.parent = self.FormToPySideWidget(form)
+        self.PopulateForm()
+
+
+    def PopulateForm(self):
+        # Create layout
+        layout = QtGui.QVBoxLayout()
+
+        layout.addWidget(
+            QtGui.QLabel("Hello from <font color=red>PySide</font>"))
+        layout.addWidget(
+            QtGui.QLabel("Hello from <font color=blue>IDAPython</font>"))
+
+        self.parent.setLayout(layout)
+
+
+    def OnClose(self, form):
+        """
+        Called when the plugin form is closed
+        """
+        pass
+
+plg = MyPluginFormClass()
+plg.Show("PySide hello world")
